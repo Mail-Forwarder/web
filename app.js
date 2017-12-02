f17c0ac9d083b855a4007677cb9923ee4392f7c0
Moquo <mail@moquo.de>
2017-12-02 14:36:31 +0100

    Added basic express stuff

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
+// ENV vars
+const PORT = process.env.MF_PORT || 9068;
+
 // Logger
 var logger = require('./logger.js');
+
+// Express basic stuff
+var express = require('express');
+var app = express();
+// Set view engine to ejs
+app.set('view engine', 'ejs');
+
+// Start web server
+app.listen(PORT, function() {
+    logger.success('Now listening on port ' + PORT);
+});
