15a77efa53dae1b55db1366575fa8d091c274e87
Moquo <mail@moquo.de>
2017-12-02 14:19:29 +0100

    Added [ERROR] logger

--- logger.js
+++ logger.js
@@ -3,8 +3,14 @@ var colors = require('colors');
 
 // [INFO] console out
 var info = function(message) {
-    console.log(colors.cyan('[INFO]'), message)
+    console.log(colors.cyan('[INFO]'), message);
+}
+
+// [ERROR] console out
+var error = function(message) {
+    console.log(colors.red('[ERROR]'), message);
 }
 
 // Module exports
 module.exports.info = info;
+module.exports.error = error;
