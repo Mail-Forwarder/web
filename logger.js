e7c98da5335b3dcd36a9169bbcc4ad81928992d7
Moquo <mail@moquo.de>
2017-12-02 14:22:26 +0100

    Added [SUCCESS] logger

--- logger.js
+++ logger.js
@@ -11,6 +11,12 @@ var error = function(message) {
     console.log(colors.red('[ERROR]'), message);
 }
 
+// [SUCCESS] console out
+var success = function(message) {
+    console.log(colors.green('[SUCCESS]'), message);
+}
+
 // Module exports
 module.exports.info = info;
 module.exports.error = error;
+module.exports.success = success;
