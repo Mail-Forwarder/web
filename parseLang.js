bcebd2c2b353513edf0e0ba83667db59a5a30683
Moquo <mail@moquo.de>
2017-12-22 16:31:10 +0100

    Created parseLang module

--- parseLang.js
+++ parseLang.js
@@ -1,6 +1,17 @@
+// replace-string
+var replaceString = require('replace-string');
+
 // Parse the lang
 var parse = function(lang) {
+    // Stringify the lang
+    var rawLang = JSON.stringify(lang);
 
+    // Replace vars
+    rawLang = replaceString(rawLang, '%name', lang.meta.name);
+
+    // JSONize and return
+    var lang = JSON.parse(rawLang);
+    return lang;
 }
 
 // module exports
