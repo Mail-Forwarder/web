4165583827562a68df778f8b6a0fa8736d0a51b1
BenKato151 <anime151@gmx.de>
2018-03-09 21:04:20 +0100

    edited for offline work

--- public/js/page.js
+++ public/js/page.js
@@ -1,8 +1,17 @@
+
 // Document ready
 $(document).ready(function() {
     // Does the side nav-ul exists = Is there a navbar?
     if(!($('#mobile-nav').length === 0)) {
         // Init side nav
         $('.button-collapse').sideNav();
+    }
+
+    // Does the side nav exists
+    if(!($('.sidenav').length === 0)) {
+        // Init side nav
+        var elem = document.querySelector('.sidenav');
+        var instance = Materialize.Sidenav.init(elem, {});
+        $('.sidenav').sidenav();
     }
 });
