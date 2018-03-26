c7cede17b7fe1a6080662e1ce9e1bb3635f1530e
BenKato151 <anime151@gmx.de>
2018-03-26 11:49:28 +0200

    deleted useless code in our page.js

--- public/js/page.js
+++ public/js/page.js
@@ -1,15 +1,11 @@
 
 // Document ready
 $(document).ready(function() {
-    // Does the side nav-ul exists = Is there a navbar?
-    if(!($('#mobile-nav').length === 0)) {
-        // Init side nav
-        $('.button-collapse').sideNav();
-    };
-    
+
     // Does the side nav exists
     if(!($('.sidenav').length === 0)) {
         // Init side nav
         $('.sidenav').sidenav();
     };
+    //next code
 });
